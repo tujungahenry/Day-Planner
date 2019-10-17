f7e121738054c0ddd69087265b4520bc9f4d6b98
Henry Tum <tujungahenry@gmail.com>
2019-10-17 16:31:30 -0700

    intial Commit

---
+++ assets/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+//Current date displayed to top of page
+var dateString = moment().format("dddd, MMMM Do YYYY, h:mm a");
+$("#currentDay").html(dateString);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
