9aacafbb06487ffa7e3b04294a7ffac2c041532a
Henry Tum <tujungahenry@gmail.com>
2019-10-19 18:07:06 -0700

    final touch up

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
 
-    //add the current date to the #current date id in the HTML//
+    //Date for heading
     var dateString = moment().format("dddd, MMMM Do YYYY, h:mm a");
     $("#currentDay").html(dateString);
     
-    //create html timeblocks with for loop starting at 9 am until 5pm//
+    //forloop for times
     var scheduledHours = [];
     
     for (var hour = 9; hour < 18; hour++) {
@@ -12,24 +12,24 @@ $(document).ready(function () {
             hour
         }).format('h  a'));
         $('.container').append(`<div class="row time-block" data-time="${hour}"> 
-           <!--hour column-->
+           <!--time-->
                <div class="col-sm col-md-2 hour"> 
                  <p class=dayHour>${moment({hour}).format('h  a')}</p>
                </div> 
                
-           <!--user input text area-->
+           <!--data-->
                <div class="col-sm col-md-8 d-flex description"> 
                  <textarea class=textArea></textarea> 
                </div> 
           
-           <!--bootstrap saveBtn-->
+           <!--save-->
                <div class="col-sm col-md-2 saveBtn">
                <i class="far fa-save fa-2x" id=icon></i>  
                </div>`);
     }
     
     
-    //Check the hour of the current day to the hour represented in the HTML data-element to decide it's background color.
+    //background color corresponding to time.
     var m = moment();
     $.each($(".time-block"), function (index, value) {
         let dateHour = $(value).attr("data-time");
@@ -42,7 +42,7 @@ $(document).ready(function () {
         }
     });
     
-    //Check for local storage to set value to the object
+    //local storage
     let timeObject = {};
       if (localStorage.getItem('timeObject')) {
           timeObject = JSON.parse(localStorage.getItem('timeObject'));
@@ -60,21 +60,18 @@ $(document).ready(function () {
         };
       }
     
-    //set value of timeObject to equal the user input for each row 
+    //user input 
     $(".time-block").each(function(){
        $(this).find(".textArea").val(timeObject[$(this).attr("data-time")].value);
-       //console.log(this)
       });
     
-     //save value to local storage on click
+     //save button
      $(".saveBtn").on('click', function(event){
-         //set timeObject time attribute
          var timeValue = $(this).closest(".time-block").attr("data-time");
-        //set timeObject value attribute
          var textValue = $(this).closest(".time-block").find(".textArea").val();
          timeObject[timeValue].value = textValue;
     
-      //save user input in each object to local storage
+      //user input  saved local storage
          localStorage.setItem('timeObject', JSON.stringify(timeObject));
     
           //console.log(textValue)
